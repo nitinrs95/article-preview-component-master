a47da298b4795e329dc42ece8d55b27f2bd02b5f
nitinrs95 <195466349+nitinrs95@users.noreply.github.com>
2025-04-28 11:08:51 +0530

    Complete Chalenge

---
+++ src/script.js
@@ -0,0 +1,8 @@
+const popEl = document.querySelector(".article__social-popup");
+const btnEl = document.querySelector(".article__share-button");
+console.log(popEl);
+
+btnEl.addEventListener("click", (e) => {
+  popEl.classList.toggle("article__social-popup--hidden");
+  btnEl.classList.toggle("btn--active");
+});
